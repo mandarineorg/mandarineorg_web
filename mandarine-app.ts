b36cf281db327d53ba639a392f211910eea5bf57
Andres Pirela <andreespirela@outlook.com>
2020-07-26 14:47:13 -0400

    Solving angular routing issue

--- mandarine-app.ts
+++ mandarine-app.ts
@@ -1,9 +1,36 @@
-import { MandarineCore } from "https://deno.land/x/mandarinets@v1.2.1/mod.ts";
+import { MandarineCore, Controller, GET, Middleware, MiddlewareTarget, ResponseParam } from "https://deno.land/x/mandarinets@master/mod.ts";
 import * as flags from "https://deno.land/std@0.61.0/flags/mod.ts";
 
-const {args} = Deno;
-const DEFAULT_PORT = 8080;
+const { args } = Deno;
+const DEFAULT_PORT = 80;
 const argPort = flags.parse(args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-new MandarineCore().MVC().run({ port: port });
+@Middleware(new RegExp('/docs/*'))
+export class Middleware1 implements MiddlewareTarget {
+
+    public onPreRequest(@ResponseParam() response: any): boolean {
+        const angularIndex = Deno.openSync("./app/mandarine-static/index.html", {read: true});
+        response.body = Deno.readAllSync(angularIndex);
+        return false;
+    }
+
+    public onPostRequest(): void {
+    }
+}
+
+@Controller()
+export class MyController {
+
+    @GET('/docs/:product/:content')
+    public handler1() {
+    }
+
+    @GET('/docs/:branch/:product/:content')
+    public handler2() {
+    }
+
+}
+
+let mvc = new MandarineCore().MVC();
+mvc.run();
