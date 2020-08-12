b638b606f53756a2ca4315025fb718665aca72ec
Andres Pirela <andreespirela@outlook.com>
2020-08-12 10:27:25 -0400

    Adding blog to menu on homepage

--- mandarine-org-web/src/app/components/navbar/navbar.component.ts
+++ mandarine-org-web/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { MandarineWeb } from '../../../mandarineWebsite.ns';
 
 @Component({
@@ -7,4 +7,21 @@ import { MandarineWeb } from '../../../mandarineWebsite.ns';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarHeader {
+  @ViewChild('ResponsiveMenuContainer')
+  private responsiveMenuContainer: ElementRef;
+
+  @ViewChild('sidenav')
+  private sidenav: any;
+
+
+  public toggleResponsiveMenu() {
+    this.sidenav.toggle();
+    if(this.responsiveMenuContainer.nativeElement.classList.contains('hide-el')) {
+      this.responsiveMenuContainer.nativeElement.classList.remove('hide-el');
+      this.responsiveMenuContainer.nativeElement.classList.add('show-el');
+    } else {
+      this.responsiveMenuContainer.nativeElement.classList.remove('show-el');
+      this.responsiveMenuContainer.nativeElement.classList.add('hide-el');
+    }
+  }
 }
